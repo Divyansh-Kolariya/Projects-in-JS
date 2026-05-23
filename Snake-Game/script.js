57a4649a8caf92577a1eb2271cb0ae7de9816730
Divyansh Kolariya <divyashkolariya67@gmail.com>
2026-05-23 13:36:54 +0530

    add

---
+++ Snake-Game/script.js
@@ -0,0 +1,65 @@
+const board = document.querySelector('.board');
+const blockSize = 50;
+const cols = Math.floor(board.clientWidth / blockSize);
+const rows = Math.floor(board.clientHeight / blockSize);
+
+const blocks = [];
+const snake = [ { row: 1, col: 2 }, { row: 1, col: 3 } ];
+let direction = 'down'
+
+// Tell the grid how many columns/rows
+board.style.setProperty('--cols', cols);
+board.style.setProperty('--rows', rows);
+
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block);
+        blocks[`${row}-${col}`] = block
+    }
+}
+
+function render(){
+    snake.forEach( gogo => {
+        blocks[ `${gogo.row}-${gogo.col}`].classList.add("fill")
+    })
+}
+
+setInterval(() => {
+    let head = null
+
+    if(direction === 'left'){
+        head = { row: snake[0].row, col: snake[0].col-1 }
+    }else if(direction === 'right'){
+        head = { row: snake[0].row, col: snake[0].col+1 }
+    }else if(direction === 'down'){
+        head = { row: snake[0].row+1, col: snake[0].col }
+    }else if(direction === 'up'){
+        head = { row: snake[0].row-1, col: snake[0].col }
+    }
+
+    snake.forEach( gogo => {
+        blocks[ `${gogo.row}-${gogo.col}`].classList.remove("fill")
+    })
+
+    snake.unshift(head)
+    snake.pop()
+
+
+    render()
+}, 400)
+
+
+addEventListener("keydown", (event) => {
+    if(event.key == "ArrowUp"){
+        direction = "up"
+    }else if(event.key == "ArrowDown"){
+        direction = "down"
+    }else if(event.key == "ArrowLeft"){
+        direction = "left"
+    }else if(event.key == "ArrowRight"){
+        direction = "right"
+    }
+    
+})
