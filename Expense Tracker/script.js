d527ab6f0522aeb4314d6e273b1bfa574b2950ff
Divyansh Kolariya <divyanshkolariya67@gmail.com>
2026-06-08 13:03:43 +0530

    feat: implement transaction management functionality in Expense Tracker

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -6,4 +6,43 @@ const transFromEl = document.getElementById("transaction-form");
 const descriptionEl = document.getElementById("description");
 const amountEl = document.getElementById("amount");
 
-let transactions = JSON.parse(localStorage.getItem("transaction"))
+let transactions = JSON.parse(localStorage.getItem("transaction")) || [];
+
+transFromEl.addEventListener("submit", addTransaction);
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    const description = descriptionEl.value.trim();
+    const amount = parseFloat(amountEl.value);
+
+    transactions.push({
+        id:Date.now(),
+        description,
+        amount
+    })
+
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+
+    updateTransactionList()
+    updateSummary()
+
+    transFromEl.reset()
+
+}
+
+function updateTransactionList(){
+    transListEl.innerHTML = ""
+
+    const sortedTransactions = [...transactions].reverse()
+
+    sortedTransactions.forEach((transaction) => {
+        const transactionEl = createTransactionElement(transaction)
+        transListEl.appendChild(transactionEl)
+    })
+}
+
+function createTransactionElement(transaction) {
+    const li = document.createElement("li")
+    li.classList.add("transaction")
+}
